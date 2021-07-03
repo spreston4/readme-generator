857d06a13f78d00869e6445e6224197268547636
spreston <sam.preston11@gmail.com>
2021-07-03 12:28:25 -0500

    Working prompt function.

--- index.js
+++ index.js
@@ -11,8 +11,22 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log('test');
-''}
+    
+    // Promt user for input
+    inquirer    
+        .prompt ([
+            {
+                type: 'input',
+                message: 'Enter the title of your project: ',
+                name: 'title',
+            },
+        ])
+
+        // Then 'generateMarkdown' & 'writeToFile'
+        .then((answers) => {
+            console.log(answers);
+        })
+}
 
 // Function call to initialize app
 init();
